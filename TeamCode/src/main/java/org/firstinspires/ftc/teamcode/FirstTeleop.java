2ead09e0fafb6c846d4ed8031ad0444fce36b898
Andrei Cionca <andreicionca1@gmail.com>
2024-01-07 01:04:52 +0200

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTeleop.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class FirstTeleop extends OpMode {
+
+    @Override
+    public void init() {
+        // Happens just once when the robot is initialized
+    }
+
+    @Override
+    public void init_loop() {
+        // Happens repeatedly during the initialization phase
+    }
+
+    @Override
+    public void start() {
+        // Happens once when the start button is pressed
+    }
+
+    @Override
+    public void loop() {
+        // Happens repeatedly after the robot is initialized
+
+    }
+
+    @Override
+    public void stop() {
+        // Happens once when the stop button is pressed
+    }
+}
